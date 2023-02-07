a95145fe56a7cd0dce723fd9f41bafbe19f41336
sskyuw <87478406+sskyuw@users.noreply.github.com>
2023-02-07 14:56:47 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        int leng = message.length();
+        
+        answer = leng*2; //가로 2cm 규격
+        return answer;
+    }
+}
