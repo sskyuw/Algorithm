18a927ba21fc91b2ab7bbd497c6a00264b93ff66
sskyuw <87478406+sskyuw@users.noreply.github.com>
2023-02-13 20:28:38 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 0;
+        
+        for (int i=0; i<t;i++)
+        {
+               //두배만큼 증식 
+                n*=2;               
+                answer = n;
+            
+            }
+        return answer;
+    }
+}
