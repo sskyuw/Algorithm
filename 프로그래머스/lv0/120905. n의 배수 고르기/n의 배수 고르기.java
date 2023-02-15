89e18da845115bb65bacf1d38162fa99fabeae4a
sskyuw <87478406+sskyuw@users.noreply.github.com>
2023-02-15 13:18:45 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> solution(int n, int[] numlist) {
+       
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+        
+        for(int i=0; i<numlist.length;i++)
+        {
+            if(numlist[i]%n == 0)
+            {
+                answer.add(numlist[i]);
+            }
+        }
+        
+        return answer;
+    }
+}
